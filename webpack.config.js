b450c5e9823d3de59cf652fc4523bd2db8acb995
Ningsong Shen <ningsongshen123@outlook.com>
2020-07-10 13:59:10 -0400

    chore: update webpack config for bare-minimum compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const webpack = require('webpack');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     hot: false,
@@ -12,59 +13,57 @@ module.exports = {
     // disableHostCheck: true,
     allowedHosts: [
       '10.0.2.2',
-    ]
+    ],
   },
   devtool: 'cheap-source-map',
   entry: [
     path.resolve(__dirname, 'app/main.js'),
     path.resolve(__dirname, 'app/stylesheets/main.scss'),
   ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: './app.min.js'
+    filename: './app.min.js',
   },
   module: {
-    loaders: [
+    rules: [
       { test: /\.css$/, include: path.resolve(__dirname, 'app'), loader: 'style-loader!css-loader' },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            { loader: 'sass-loader', query: { sourceMap: false } },
-          ],
-          publicPath: '../'
-        }),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader, 
+            options: {
+              publicPath: '../',
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
       { test: /\.js[x]?$/, include: [
         path.resolve(__dirname, 'app'),
-      ], exclude: /node_modules/, loader: 'babel-loader' }
-    ]
+      ], exclude: /node_modules/, loader: 'babel-loader' },
+    ],
   },
   resolve: {
     extensions: ['.js', '.jsx', '.css', '.scss'],
     alias: {
       stylekit: path.join(__dirname, 'node_modules/sn-stylekit/dist/stylekit.css'),
-    }
+    },
   },
   plugins: [
-    new ExtractTextPlugin({ filename: './app.css', disable: false, allChunks: true}),
-    new uglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
+    new MiniCssExtractPlugin({ filename: './app.css'}),
+    new UglifyJsPlugin({}),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './app/index.html', to: 'index.html' },
+        { from: './node_modules/sn-editor-kit/dist/filesafe-js/EncryptionWorker.js', to: 'filesafe-js/EncryptionWorker.js' },
+      ],
     }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    new CopyWebpackPlugin([
-      { from: './app/index.html', to: 'index.html' },
-      { from: './node_modules/sn-editor-kit/dist/filesafe-js/EncryptionWorker.js', to: 'filesafe-js/EncryptionWorker.js' },
-    ])
-  ]
+  ],
 };
